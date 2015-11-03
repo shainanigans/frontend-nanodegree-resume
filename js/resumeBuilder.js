fbd3010c8962c85075b7e7c228fdd8261087aa26
shainanigans <me@shainakoval.com>
2015-11-03 16:11:35 +1100

    Clean resumeBuilder.js with new objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,44 +1,98 @@
 var bio = {
     "name": "Shaina Koval",
-    "role": "Designer and Front-End Dev",
-    "contact": {
-        "email": "me@examle.com",
+    "role": "Designer and Front-End Developer",
+    "contacts": {
+        "email": "me@shainakoval.com",
+        "website": "http://www.shainakoval.com",
         "github": "shainanigans",
         "location": "Sydney"
     },
     "picture": "images/shaina-smile-crop.png",
-    "welcome": "It's nice to meet you.",
+    "welcomeMessage": "It's nice to meet you.",
     "skills": [
         "Graphic Design",
         "Web Design",
         "HTML",
         "CSS",
+        "WordPress",
+        "PHP",
+        "MailChimp",
         "Photoshop",
         "InDesign",
-        "Illustrator"
+        "Illustrator",
+        "Microsoft Office",
+        "Mac OSX",
+        "Windows",
+        "Time Management",
+        "Communication",
+        "Organisation"
     ]
 }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-var formattedContact = HTMLcontactGeneric.replace("%contact%",bio.contact);
-var formattedPicture = HTMLbioPic.replace("%data%",bio.picture);
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcome);
-var formattedSkills = HTMLskills.replace("%data%",bio.skills);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-//$("#main").append(formattedContact);
-$("#main").append(formattedPicture);
-$("#main").append(formattedWelcome);
-//$("#main").append(formattedSkills);
-
 var work = {
-    "position": "Designer and Front-End Dev",
-    "employer": "Shaina Koval Design & Illustration",
-    "years": 5,
-    "city": "Everywhere"
+    "jobs": [
+        {
+            "title": "Designer, Front-End Developer and Illustrator",
+            "employer": "Shaina Koval Design & Illustration",
+            "dates": "Dec 2010 - Present",
+            "location": "NYC, San Francisco, Christchurch and Sydney",
+            "description": "By managing my time effectively, I have continued freelance work on design and illustration projects while working full- time positions. My freelance work has included: New company branding projects involving web design and development, logo design, business cards, vehicle graphics, and more. Illustrations for zines, posters, merchandising, essays. Self-promotion on my personal website and Facebook. Managing projects and client relations."
+        },
+        {
+            "title": "Graphics Design and Digital Media Exec",
+            "employer": "Central Heating New Zealand",
+            "dates": "Apr 2013 - Jun 2015",
+            "location": "Christchurch, New Zealand",
+            "description": "As the primary member of the design and marketing team, I was responsible for everything design and marketing-related and consistently juggled a combination of long-term, short- term and urgent projects simultaneously. Redesigned the company website by overhauling an incomplete custom WordPress theme with CSS and PHP, new plugins, and CRM integration. Customised page templates for implementation of the WooCommerce eCommerce plugin for company’s catalogue of over 500 products. Wrote SEO friendly copy for new pages. Edited images for use on site. Later evolved the site to a responsive design. Managed website new content and updates, provided monthly Google Analytics report summaries, and made changes to better improve the site UX. Updated the company’s visual identity including brochures, catalogues, internal documents, etc. Designed and developed templates, wrote content, managed campaigns for EDM using MailChimp.Designed and wrote copy for print advertisements for B2B and B2C magazines and newspapers. Wrote articles and case studies targeted at the retail, plumbing and architecture markets. Explained rationale for concepts and technologies in laymen’s terms to relevent team members. Liaised with printing companies, consultants, and coworkers as required to get projects done efficiently. Resolved last-minute emergencies like website going down at 5pm on a Friday."
+        },
+        {
+            "title": "Design and Marketing Manager",
+            "employer": "AcademyX",
+            "dates": "Dec 2012 - Mar 2013",
+            "location": "San Francisco, CA, USA (working remotely from New Zealand)",
+            "description": "In this role, I designed all company advertisements and promotional materials, providing a cohesive aesthetic for the company. Additionally, I managed website updates to content and code and executed online marketing strategies. Designed and coded e-newsletters using HTML and CSS in Dreamweaver, ensuring that code would translate properly into a variety of email clients. Researched topics and wrote content for e-newsletters. • Updated web pages with content changes, added rich snippets, and unified styling for over 500 pages. Rewrote and coded web page content for better SEO. Increased Facebook page activity with engaging content."
+        },
+        {
+            "title": "Training and Operations Coordinator",
+            "employer": "AcademyX",
+            "dates": "Jun 2011 - Dec 2012",
+            "location": "San Francisco, CA, USA",
+            "description": "This role fully utilised my attention to detail, making sure that all of our San Francisco branch classes met company standards for excellence. In a small office, it was essential that I gave 110% each day, ensured that my work was up to par, and wore as many hats as the day required, as any mistake could cost the company thousands of dollars. I directly supported the CEO in day-to-day operations as well as long term growth projects, design tasks, and customer retention, and assigned responsibilities as needed to the Administrative Assistant. Managed over 100 instructors and assigned teaching responsibilities to them. Scheduled public enrolment and private computer training classes, ensuring that all computer equipment was functional and that sufficient training materials were prepared. Designed new brochure, book cover, class catalogue, flyers, and banners. Promoted and maintained government funded training programs by assisting customers and working with government liaisons. Managed emergencies such as whe an instructor failed to show up for a full class at 9am (where the students have paid $400 USD to attend) or when a student became violently ill during a class. Answered questions and mediated customer and instructor concerns as lead customer service representative and go-to employee for knowledge on our services and operations. Averaged over 70 emails per day in addition to other responsibilities."
+        },
+        {
+            "title": "Administrative Assistant",
+            "employer": "AcademyX",
+            "dates": "Feb 2011 - May 2011",
+            "location": "San Francisco, CA, USA",
+            "description": "As the first face that customers saw when they walked in, I set the tone for an enjoyable experience at the company’s facilities. I assisted all other staff members, from accountant to CEO, with their administrative needs while providing customer service and light IT support in the classroom. Addressed customer enquiries and concerns. Organised office storage space, maintained clean and professional classrooms, and stocked supplies. Consulted training calendar and enrolment lists to stock books and class materials for all classes on site and off site. Installed software and solved technical issues ranging from network connectivity to malfunctioning hardware to computer re-imaging. ReceivedEmployeeoftheMonththeonlymonththisaward was offered in my almost two years at the company (and was allowed to keep the certificate displayed at my desk for my entire tenure there)."
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "Shaina Koval Design & Illustration",
+            "dates": "Dec 2010 - Present",
+            "images": [
+                "images/shaina-website-home-desktop.png",
+                "images/shaina-website-project-desktop.png",
+                "images/shaina-website-home-mobile.png"
+            ],
+            "description": "Design and illustration work"
+        },
+        {
+            "title": "Example",
+            "dates": "Dates",
+            "images": [
+                "images/shaina-website-home-desktop.png",
+                "images/shaina-website-project-desktop.png",
+                "images/shaina-website-home-mobile.png"
+            ],
+            "description": "Example of stuff"
+        }
+    ]
+
 }
 
 var education = {
@@ -46,7 +100,7 @@ var education = {
         {
             "name": "Vassar College",
             "dates": "August 2007 - June 2009",
-            "city": "Poughkeepsie, NY, USA",
+            "location": "Poughkeepsie, NY, USA",
             "url": "http://www.vassar.edu"
         },
         {
@@ -54,19 +108,40 @@ var education = {
             "graduation": "February 2010",
             "degree": "BFA",
             "major": "Communications Design",
-            "city": "Brooklyn, NY, USA",
+            "location": "Brooklyn, NY, USA",
             "url": "http://www.pratt.edu"
         }
     ],
-    "onlineCourses": [
+    "training": [
         {
             "title": "Front-End Web Developer Nanodegree",
             "school": "Udacity",
-            "date": 2015,
+            "date": "Oct 2015 - Present",
             "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+        },
+        {
+            "title": "PHP",
+            "school": "Codecademy",
+            "date": "Present",
+            "url": "https://www.codecademy.com/learn/php"
+        },
+        {
+            "title": "Responsive Web Design",
+            "school": "Dynamic Web Training",
+            "date": "Jan 2015",
+            "url": "https://www.dynamicwebtraining.com.au/mobile-website-and-app-training/responsive-web-design-course"
+        },
+        {
+            "title": "InDesign Beyond the Basics",
+            "school": "Yoobee School of Design",
+            "date": "Sep 2014",
+            "url": "http://www.yoobee.ac.nz/study/graphic-design/indesign-beyond-the-basics/"
+        },
+        {
+            "title": "24-Evening Online Marketing",
+            "school": "AcademyX",
+            "date": "Oct - Dec 2012",
+            "url": "https://www.academyx.com/training/san_francisco/programs/web-marketing/"
         }
     ]
 }
-
-$("#main").append(work["position"]);
-$("#main").append(education.name);
