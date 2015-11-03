a5fe4fbcbaea4c3782718cf975bb0a6ad71e5c24
shainanigans <me@shainakoval.com>
2015-11-03 12:25:59 +1100

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,21 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name="Shaina Koval";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role="Designer and Front-End Dev";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["Graphic Design", "Web Design", "HTML","CSS","Photoshop", "InDesign", "Illustrator"]
+
+var bio = {
+    "name": "Shaina Koval",
+    "role": "Designer and Front-End Dev",
+    "contactInfo": "www.shainakoval.com",
+    "pictureURL": "images/shaina-smile-crop.png",
+    "welcomeMessage": "It's nice to meet you.",
+    "skills": skills
+}
+
+$("#main").append(bio.name);
