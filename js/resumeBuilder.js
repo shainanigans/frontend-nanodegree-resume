732f61f9c4d929b1b69dbc3e197b92cd41217c02
shainanigans <me@shainakoval.com>
2015-11-04 20:51:39 +1100

    Add encapsulating function for projects and work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -70,6 +70,27 @@ var work = {
     ]
 }
 
+work.display = function() {
+    for (job in work.jobs) {
+        if (work.jobs.hasOwnProperty(job)) {
+            $("#workExperience").append(HTMLworkStart);
+
+            var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+            var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+            var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+            var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+
+            $(".work-entry:last").append(formattedEmployer + formattedTitle);
+            $(".work-entry:last").append(formattedLocation);
+            $(".work-entry:last").append(formattedDates);
+            $(".work-entry:last").append(formattedDescription);
+        }
+    }
+}
+
+work.display();
+
 var projects = {
     "projects": [
         {
@@ -95,6 +116,32 @@ var projects = {
     ]
 
 }
+
+projects.display = function() {
+    for (project in projects.projects) {
+        if (projects.projects.hasOwnProperty(project)) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+            $(".project-entry:last").append(formattedTitle);
+
+            var formattedDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+            $(".project-entry:last").append(formattedDates);
+
+            var formattedDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+            $(".project-entry:last").append(formattedDescription);
+
+            if (projects.projects[project].images.length > 0) {
+                for (image in projects.projects[project].images) {
+                    var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+                    $(".project-entry:last").append(formattedImage);
+                }
+            }
+        }
+    }
+}
+
+projects.display();
 
 var education = {
     "schools": [
@@ -166,25 +213,6 @@ if (bio.skills.length > 0) {
     }
 }
 
-function displayWork() {
-    for (job in work.jobs) {
-        if (work.jobs.hasOwnProperty(job)) {
-            $("#workExperience").append(HTMLworkStart);
-
-            var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-            var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
-            var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
-            var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
-            var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
-
-            $(".work-entry:last").append(formattedEmployer + formattedTitle);
-            $(".work-entry:last").append(formattedLocation);
-            $(".work-entry:last").append(formattedDates);
-            $(".work-entry:last").append(formattedDescription);
-        }
-    }
-}
-
 //Log Clicks
 $(document).click(function(loc) {
     var x = loc.pageX;
@@ -192,13 +220,3 @@ $(document).click(function(loc) {
 
     logClicks(x,y);
 });
-
-//Internationalise Button - This is probably not necessary in project
-$("#main").append(internationalizeButton);
-
-function inName() {
-    var names = bio.name.split(" ");
-    var formattedNames = names[0].charAt(0).toUpperCase() + names[0].slice(1).toLowerCase() + " " + names[1].toUpperCase();
-
-    return formattedNames;
-}
